2ea499d8ba4a080a813fbf9f767a182f7291c6f6
Ethan Peterson <17emp5@queensu.ca>
2019-12-02 16:55:18 -0500

    Scaffold Library for reading ECU CAN

---
+++ CAN/QCAN/QCAN.cpp
@@ -0,0 +1,6 @@
+#include "Arduino.h"
+#include "QCAN.h"
+
+void QCAN::QCAN() {}
+
+void QCAN::update() {}
---
+++ CAN/QCAN/QCAN.h
@@ -0,0 +1,12 @@
+#ifndef QCAN_h
+#define QCAN_h
+
+#include "Arduino.h"
+
+class QCAN {
+ public:
+  QCAN();
+  void update();
+};
+
+#endif
