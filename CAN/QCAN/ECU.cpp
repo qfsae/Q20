b0ed944387095689a323e17a5179b65af95c422c
Ethan Peterson <17emp5@queensu.ca>
2019-12-24 23:16:09 -0500

    Bundle scaling into read functions

--- CAN/QCAN/ECU.cpp
+++ CAN/QCAN/ECU.cpp
@@ -11,19 +11,17 @@
 ECU::ECU() {}
 void ECU::begin(MCP_CAN CAN) { _CAN = CAN; }
 
-int readUnsigned(unsigned char lowByte, unsigned char highByte) {
-  return (highByte * 256) + lowByte;
+int readUnsigned(unsigned char lowByte, unsigned char highByte, float factor) {
+  return ((highByte * 256) + lowByte) * factor;
 }
 
-int readSigned(unsigned char lowByte, unsigned char highByte) {
+int readSigned(unsigned char lowByte, unsigned char highByte, float factor) {
   int reading = (highByte * 256) + lowByte;
   if (reading > 32767) {
     reading -= 65536;
   }
-  return reading;
+  return reading * factor;
 }
-
-float scaleReading(int reading, float factor) { return reading * factor; }
 
 // Updates the public variables on the object when a new message is received
 // Running this update function in the loop of an Arduino sketch gives easy
@@ -48,35 +46,33 @@ void ECU::update() {
     switch (id) {
     case PE1:
       // This message provides RPM, TPS, fuel open time and ignition angle
-      this->rpm = readUnsigned(msgBuf[0], msgBuf[1]);
-      this->tps = scaleReading(readSigned(msgBuf[2], msgBuf[3]), 0.1);
-      this->openTime = scaleReading(readSigned(msgBuf[4], msgBuf[5]), 0.1);
-      this->ignitionAngle = scaleReading(readSigned(msgBuf[6], msgBuf[7]), 0.1);
+      this->rpm = readUnsigned(msgBuf[0], msgBuf[1], 1);
+      this->tps = readSigned(msgBuf[2], msgBuf[3], 0.1);
+      this->openTime = readSigned(msgBuf[4], msgBuf[5], 0.1);
+      this->ignitionAngle = readSigned(msgBuf[6], msgBuf[7], 0.1);
       break;
     case PE2:
-      this->pressure = scaleReading(readSigned(msgBuf[0], msgBuf[1]), 0.01);
-      this->MAP = scaleReading(readSigned(msgBuf[2], msgBuf[3]), 0.01);
-      this->lambda = scaleReading(readSigned(msgBuf[4], msgBuf[5]), 0.01);
-      this->pressureUnit = readUnsigned(msgBuf[6], msgBuf[7]);
+      this->pressure = readSigned(msgBuf[0], msgBuf[1], 0.01);
+      this->MAP = readSigned(msgBuf[2], msgBuf[3], 0.01);
+      this->lambda = readSigned(msgBuf[4], msgBuf[5], 0.01);
+      this->pressureUnit = readUnsigned(msgBuf[6], msgBuf[7], 1);
       break;
     case PE3:
       // Provides readings on first 4 analog inputs
       for (int i = 0; i < 8; i += 2) {
-        this->analogInputs[i / 2] =
-            scaleReading(readSigned(msgBuf[i], msgBuf[i + 1]), 0.001);
+        this->analogInputs[i / 2] = readSigned(msgBuf[i], msgBuf[i + 1], 0.001);
       }
       break;
     case PE4:
       // Provides readings for last 4 analog inputs
       for (int i = 0; i < 8; i += 2) {
         this->analogInputs[4 + i / 2] =
-            scaleReading(readSigned(msgBuf[i], msgBuf[i + 1]), 0.001);
+            readSigned(msgBuf[i], msgBuf[i + 1], 0.001);
       }
       break;
     case PE5:
       for (int i = 0; i < 8; i += 2) {
-        this->frequencies[i / 2] =
-            scaleReading(readSigned(msgBuf[i], msgBuf[i + 1]), 0.2);
+        this->frequencies[i / 2] = readSigned(msgBuf[i], msgBuf[i + 1], 0.2);
       }
       break;
     case PE6:
