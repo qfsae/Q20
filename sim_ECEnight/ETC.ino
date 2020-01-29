4da792c835e81293708966f9ceb8be338662aa1d
Brent Champion <brent.champion@queensu.ca>
2020-01-28 21:02:51 -0500

    adds blipping and control between sim and pedal, as well as all prev inputs

--- sim_ECEnight/ETC.ino
+++ sim_ECEnight/ETC.ino
@@ -13,10 +13,19 @@
 MCP_CAN CAN(SPI_CS_PIN);
 
 // DOUBLE CHECK ALL OF THIS
+const int APPS1Pin = A0;
+const int APPS2Pin = A1;
+const int BrakePres = A2;
 const int TPS1Pin = A3;  //Pin of TPS1
-const int TPS2Pin = A5;  //Pin of TPS2
+const int TPS2Pin = A4;  //Pin of TPS2
 const int torquePin = 9; //PWM pin to motor driver
 const int directionPin = 4; //directionPin
+
+const int upButton = 2;
+const int downButton = 3;
+const int shiftCut = 10;
+const int upLED = 11;
+const int downLED = 12;
 
 int rpm;
 float batVolt = 13.22;
@@ -56,27 +65,56 @@ PID myPID(&Input, &OutputTorque, 0, Kp, Ki, Kd, DIRECT);
 
 void setup() {
   Serial.begin(115200);
-  while (CAN.begin(CAN_500KBPS) != CAN_OK) {
-    delay(100);
-  }
-  TCCR0B = TCCR0B & B11111000 | B00000010; // for PWM frequency of 7812.50 Hz
+  pinMode(shiftCut, OUTPUT);
+  pinMode(upLED, OUTPUT);
+  pinMode(downLED, OUTPUT);
+  digitalWrite(shiftCut, HIGH);
+  digitalWrite(upLED, HIGH);
+  digitalWrite(downLED, HIGH);
 
+  while (CAN.begin(CAN_500KBPS) != CAN_OK) { delay(100); }
+
+  digitalWrite(shiftCut, LOW);
+  digitalWrite(upLED, LOW);
+  digitalWrite(downLED, LOW);
+
+  TCCR0B = TCCR0B & B11111000 | B00000010; // for PWM frequency of 7812.50 Hz
+  pinMode(upButton, INPUT);
+  pinMode(downButton, INPUT);
+  attachInterrupt(digitalPinToInterrupt(downButton), downshift, FALLING);
+  attachInterrupt(digitalPinToInterrupt(upbutton), upshift, FALLING);
+  
+  // ETC Inputs
+  pinMode(APPS1Pin, INPUT);
+  pinMode(TPS1Pin, INPUT);
+  pinMode(TPS2Pin, INPUT);
+  pinMode(BrakePres, INPUT);
+  pinMode(BSPDFlag, INPUT);
+
+  // ETC Outputs
+  pinMode(directionPin, OUTPUT);
+  pinMode(torquePin, OUTPUT);
+  pinMode(13, OUTPUT);
   // Set PID inputs
   Input = 0;
   DesiredPosition = 0;
   myPID.SetMode(AUTOMATIC);
 
-  pinMode(TPS1Pin, INPUT);
-  pinMode(TPS2Pin, INPUT);
-  pinMode(directionPin, OUTPUT);
-  pinMode(torquePin, OUTPUT);
-  pinMode(13, OUTPUT);
+
   myPID.SetSampleTime(10);
 }
 
 int lastGear = 0;
 
 void loop() {
+    BSPD = analogRead(BSPDFlag);
+    if(BSPDFlag > 410){ flag_start = millis(); }
+    if(BSPDFlag > 410 && millis() > flag_start + 1000){
+      brickProgram();
+    }
+    Brake = analogRead(BrakePres);
+    Brake = map(Brake, BrakeLow, BrakeHigh, 0, 100); // Map to 0-100%
+
     // Read from CAN shield. 
     if (lastGear >  gear && gear != 0){ // downshift
       downshift();
@@ -85,13 +123,24 @@ void loop() {
       upshift();
     }
   
-  TPS1 = analogRead(TPS1Pin);
-  Input = map(TPS1, TPS1Close, TPS1Open, 0, 100); // Map it to a number between 0 and 100%
+  TPS1 = constrain(analogRead(TPS1Pin), TPS1Open, TPS1Close);
+  TPS2 = constrain(analogRead(TPS2Pin), TPS2Close, TPS2Open);
+  TPSDiff = (TPS1Close - TPS1Open) - ((TPS1 - TPS1Open) + (TPS2 - TPS2Close));
+  if(TPSdiff > ((TPS1Close - TPS1Open)*0.1)){
+    // differ more than 10%
+    analogWrite(torquePin, 0); // kill throttle body, this should be better
+  }
 
-  APPS1 = tps*100; // It would read this from CAN
+  Input = map(TPS1, TPS1Close, TPS1Open, 0, 1024); // Map it to a number between 0 and 100%
+  if(SIM){
+    // put stuff to receive from CAN here
+    receiveCAN();
+    APPS1 = tps*100; // It would read this from CAN
+  } else {
+    APPS1 = map(analogRead(APPS1Pin), APPS1Close, APPS1Open, 0, 1024);
+  }
 
   if (APPS1 < 0){ APPS1 = 0; }
-
   Input = APPS1 - Input;
   if(Input < 0){
     myPID.SetControllerDirection(DIRECT);
@@ -109,16 +158,73 @@ void loop() {
   lastGear = gear;
 }
 
+void receiveCAN(){
+  // Need to receive tps and gear over CAN
+}
+
+void transmitCAN(){
+  // Need to transmit TPS, Brake, Error, Flag, etc)
+
+}
+
+void brickProgram(){
+  while(1){
+    // Rip
+  }
+}
+
 void downshift(){
-  
+  digitalWrite(shiftCut, HIGH);
+  digitalWrite(downLED, HIGH);
+  int startingTPS = TPS1;
+  int startAPPS = APPS1;
+  while(TPS1 > TPS1Open + 50){
+    Input = map(TPS1, TPS1Close, TPS1Open, 0, 1024){
+    DesiredPosition = TPS1Open;
+    myPID.SetControllerDirection(REVERSE);
+    digitalWrite(directionPin, HIGH);
+    myPID.Compute();
+    analogWrite(torquePin, OutputTorque);
+    TPS1 = constrain(analogRead(TPS1Pin), TPS1Open, TPS1Close);
+    }
+  } delay(60);
+  while(TPS1 < startingTPS){
+    Input = map(TPS1, TPS1Close, TPS1Open, 0, 1024);
+    DesiredPosition = startAPPS;
+    myPID.SetControllerDirection(DIRECT);
+    digitalWrite(directionPin, LOW);
+    myPID.Compute();
+    analogWrite(torquePin, OutputTorque);
+    TPS1 = constrain(analogRead(TPS1Pin), TPS1Open, TPS1Close);
+  }
+  digitalWrite(shiftCut, LOW);
+  digitalWrite(downLED, LOW);
 }
 
 void upshift(){
-  
+  digitalWrite(shiftCut, HIGH);
+  digitalWrite(downLED, HIGH);
+  int startingTPS = TPS1;
+  int startAPPS = APPS1;
+  while(TPS1 < TPS1Close - 50){
+    Input = map(TPS1, TPS1Close, TPS1Open, 0, 1024){
+    DesiredPosition = TPS1Close;
+    myPID.SetControllerDirection(DIRECT);
+    digitalWrite(directionPin, LOW);
+    myPID.Compute();
+    analogWrite(torquePin, OutputTorque);
+    TPS1 = constrain(analogRead(TPS1Pin), TPS1Open, TPS1Close);
+    }
+  } delay(60);
+  while(TPS1 < startingTPS){
+    Input = map(TPS1, TPS1Close, TPS1Open, 0, 1024);
+    DesiredPosition = startAPPS;
+    myPID.SetControllerDirection(REVERSE);
+    digitalWrite(directionPin, HIGH);
+    myPID.Compute();
+    analogWrite(torquePin, OutputTorque);
+    TPS1 = constrain(analogRead(TPS1Pin), TPS1Open, TPS1Close);
+  }
+  digitalWrite(shiftCut, LOW);
+  digitalWrite(upLED, LOW);
 }
-
-//void downshift(){
-//  
-//  myPID.Compute();
-//  analogWrite(torquePin, OutputTorque);
-//}
--- sim_ECEnight/steering.ino
+++ sim_ECEnight/steering.ino
@@ -1,17 +1,16 @@
 /* Sample code for Steering Wheel Electronics with data from CAN from assetto 
 
 - Need to add 14-seg to this
-- Need to add Neopixels to this
+- Also add potentiometer for neopixels
 */
 
+#include <Adafruit_NeoPixel.h>
 #include "mcp_can.h"
 #include <SPI.h>
 
-##include "mcp_can.h"
-#include <SPI.h>
+#define SPI_CS_PIN 10
 
-#define SPI_CS_PIN 9 // double check if 9 or 10 for specific shield
-
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);   // Init NeoPixel Strip
 MCP_CAN CAN(SPI_CS_PIN);
 
 unsigned long rpmID = 0x00;
@@ -64,27 +63,26 @@ void loop() {
 }
 
 void mapShiftLights(){
-      if(rpm < 1000 && rpm > 500)  strip.setPixelColor(0, 0, 255, 0);      // last interval show all lights red
-     if(rpm < 1500) strip.setPixelColor(1, 0, 255, 0);
-  if(rpm < 2000) strip.setPixelColor(2, 0, 255, 0);
-  if(rpm < 2500) strip.setPixelColor(3, 0, 255, 0);
-  if(rpm < 3000) strip.setPixelColor(4, 0, 255, 0);
-  if(rpm < 3500) strip.setPixelColor(5, 0, 0, 255);
-  if(rpm < 4000) strip.setPixelColor(6, 0, 0, 255);
-  if(rpm < 4500) strip.setPixelColor(7, 0, 0, 255);
-  if(rpm < 5000) strip.setPixelColor(8, 0, 0, 255);
-  if(rpm < 5500) strip.setPixelColor(9, 0, 0, 255);
-  if(rpm < 6000) strip.setPixelColor(10, 255, 0, 0);
-  if(rpm < 6500) strip.setPixelColor(11, 255, 0, 0);
-  if(rpm < 7000) strip.setPixelColor(12, 255, 0, 0);
-  if(rpm < 7500) strip.setPixelColor(13, 255, 0, 0);
-  if(rpm < 8000) strip.setPixelColor(14, 255, 0, 0);
-  if(rpm < 8500) batchPixels(0, 15, 255, 0, 0);
-
-  strip.show();
+    if(rpm < 1000 && rpm > 500)  strip.setPixelColor(0, 0, 255, 0);      // last interval show all lights red
+    if(rpm < 1500) strip.setPixelColor(1, 0, 255, 0);
+    if(rpm < 2000) strip.setPixelColor(2, 0, 255, 0);
+    if(rpm < 2500) strip.setPixelColor(3, 0, 255, 0);
+    if(rpm < 3000) strip.setPixelColor(4, 0, 255, 0);
+    if(rpm < 3500) strip.setPixelColor(5, 0, 0, 255);
+    if(rpm < 4000) strip.setPixelColor(6, 0, 0, 255);
+    if(rpm < 4500) strip.setPixelColor(7, 0, 0, 255);
+    if(rpm < 5000) strip.setPixelColor(8, 0, 0, 255);
+    if(rpm < 5500) strip.setPixelColor(9, 0, 0, 255);
+    if(rpm < 6000) strip.setPixelColor(10, 255, 0, 0);
+    if(rpm < 6500) strip.setPixelColor(11, 255, 0, 0);
+    if(rpm < 7000) strip.setPixelColor(12, 255, 0, 0);
+    if(rpm < 7500) strip.setPixelColor(13, 255, 0, 0);
+    if(rpm < 8000) strip.setPixelColor(14, 255, 0, 0);
+    if(rpm < 8500) batchPixels(0, 15, 255, 0, 0);
+    strip.show();
 }
 
 void mapGear(){
-
+    // Add code to map to the 14-segment display
 }
 
