5d88ae8a1aad4d43b66ea56cacf734471cb08096
Brent Champion <brent.champion@queensu.ca>
2020-02-02 15:15:15 -0500

    adds damiens neopixel patterns

--- sim_ECEnight/steering/steering.ino
+++ sim_ECEnight/steering/steering.ino
@@ -17,11 +17,11 @@ Adafruit_NeoPixel strip(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 // Use pin 10 for CAN shield version 1.2
 #define SPI_CS_PIN 10
-
+#define TOGGLE 2
 int rpm;
 int gear;
 int tps;
-int pattern = 0;
+volatile int pattern = 0;
 
 MCP_CAN CAN(SPI_CS_PIN);
 
@@ -33,13 +33,17 @@ void setup() {
     Serial.println("TRY AGAIN");
     delay(1);
   }
+  pinMode(TOGGLE, INPUT);
+//  attachInterrupt(digitalPinToInterrupt(TOGGLE), cycle, RISING);
   Serial.println("Initialization Success");
 }
 
 void loop() {
+  mapPattern();
   strip.clear();
   updateCAN();
   mapShiftLights();
+//  mapGear();
 }
 
 int LED_COUNT = NUMPIXELS;
@@ -48,14 +52,15 @@ void mapShiftLights()
 {
     int brightMax = 255;
     int brightData = analogRead(brightnessPin);
-//    int brightness = map(brightData, 0, 1023, 0, brightMax);
-    int brightness = 255;
-    int maxRPM = 7000;
+    int brightness = map(brightData, 0, 1023, 0, brightMax);
+    //int brightness = 255;
+    int maxRPM = 6000;
     int RPMi = map(rpm, 0, maxRPM, 0, NUMPIXELS - 1);
 
     switch (pattern)
     {
-    case 0: // Normal shift light pattern that we already have working
+    case 0:        
+        // Normal shift light pattern that we already have working
         for (int i = 0; i < RPMi; i++)
         {
             if (i < LED_COUNT/3) // First third
@@ -64,17 +69,132 @@ void mapShiftLights()
             }
             else if (i < (2*LED_COUNT)/3) // Second third
             {
-                strip.setPixelColor(i, brightness, 0, 0); // Red
+                strip.setPixelColor(i, 0, 0, brightness); // Red
             }
             else // Last third
             {
-                strip.setPixelColor(i, 0, 0, brightness); // Blue
+                strip.setPixelColor(i, brightness, 0, 0); // Blue
             }
         }
         break;
-    case 1: // Want this to progress inwards
-        break;
-    default:
+     case 1: // Want this to progress inwards
+      strip.clear();
+          if (rpm > 1250){
+            strip.setPixelColor(0, brightness, 0, 0);//red
+            strip.setPixelColor(15, brightness, 0, 0);//red
+          }
+         if (rpm > 1600){
+            strip.setPixelColor(1, brightness, 0, 0);//red
+            strip.setPixelColor(14, brightness, 0, 0);//red
+          }
+         if (rpm > 2000){
+          strip.setPixelColor(2, brightness, 0, 0);//red
+          strip.setPixelColor(13, brightness, 0, 0);//red
+         }
+         if (rpm > 2500){
+          strip.setPixelColor(3, 0, brightness, 0);//green
+          strip.setPixelColor(12, 0, brightness, 0);//green
+         }
+          if (rpm > 3500){
+          strip.setPixelColor(4, 0, brightness, 0);//green
+          strip.setPixelColor(11, 0, brightness, 0);//green
+         }
+         if (rpm > 4000){
+          strip.setPixelColor(5, 0, brightness, 0);//green
+          strip.setPixelColor(10, 0, brightness, 0);//green
+         }
+         if (rpm > 4500){
+          strip.setPixelColor(6, 0, 0, brightness);//blue
+          strip.setPixelColor(9, 0, 0, brightness);//blue
+         }
+         if (rpm > 5000){
+          strip.setPixelColor(7, 0, 0, brightness);//blue
+          strip.setPixelColor(8, 0, 0, brightness);//blue
+         }
+
+       
+      //  break;
+    default: if (rpm > 6000){
+        for(int i = 0; i < NUMPIXELS; i++){
+          strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+        if (rpm > 5700){
+        for(int i = 0; i < NUMPIXELS - 1; i++){
+          strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+        if (rpm > 5400){
+        for(int i = 0; i < NUMPIXELS - 2; i++){
+          strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+        if (rpm > 5100){
+          for(int i = 0; i < NUMPIXELS - 3; i++){
+            strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+        if (rpm > 4800){
+          for(int i = 0; i < NUMPIXELS - 4; i++){
+            strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+        if (rpm > 4500){
+          for(int i = 0; i < NUMPIXELS - 5; i++){
+            strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+         if (rpm > 4200){
+          for(int i = 0; i < NUMPIXELS - 6; i++){
+            strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+         if (rpm > 3900){
+          for(int i = 0; i < NUMPIXELS - 7; i++){
+            strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+         if (rpm > 3600){
+          for(int i = 0; i < NUMPIXELS - 8; i++){
+            strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+         if (rpm > 3300){
+          for(int i = 0; i < NUMPIXELS - 9; i++){
+            strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+        if (rpm > 3000){
+          for(int i = 0; i < NUMPIXELS -  10; i++){
+            strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+        if (rpm > 2600){
+          for(int i = 0; i < NUMPIXELS -  11; i++){
+            strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+        if (rpm > 2100){
+          for(int i = 0; i < NUMPIXELS -  12; i++){
+            strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+        if (rpm > 1800){
+          for(int i = 0; i < NUMPIXELS -  13; i++){
+            strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+        if (rpm > 1500){
+          for(int i = 0; i < NUMPIXELS -  14; i++){
+            strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+        if (rpm > 1250){
+          for(int i = 0; i < NUMPIXELS -  15; i++){
+            strip.setPixelColor(i, 255, 255, 255);
+          }
+        }
+       
         break;
     }
     strip.show();
@@ -100,9 +220,25 @@ void updateCAN(){
     // node
   rpm = buf[0] + 256*buf[1];
   gear = buf[3];
-  Serial.print(rpm);
-  Serial.print("\t");
-  Serial.print(gear);
-  Serial.println();
+//  Serial.print(rpm);
+//  Serial.print("\t");
+//  Serial.print(gear);
+//  Serial.println();
+  }
+}
+
+void cycle(){
+  Serial.print("Cycling Pattern");
+  //pattern = pattern++ % 2;
+}
+
+void mapPattern(){
+  int pot2Volts = analogRead(A1);
+  if (pot2Volts < 500){
+    //use case 0
+    pattern = 0;
+  }
+  else {
+    pattern = 1;
   }
 }
