584e2dd5886ccfa3102bca0d66151c9fe7e75066
Ethan Peterson <17emp5@queensu.ca>
2020-02-26 22:43:53 -0500

    Add CAN packet sniffer for new Motec ECU

---
+++ CAN/motec-sniffer/motec-sniffer.ino
@@ -0,0 +1,35 @@
+#include "mcp_can.h"
+
+// Use pin 10 for CAN shield version 1.2
+#define SPI_CS_PIN 9
+// NOTE: Base Address for MOTEC M150 CAN is 0x07F0
+
+MCP_CAN CAN(SPI_CS_PIN);
+
+unsigned long id;
+unsigned char buf[8];
+unsigned char len;
+
+void setup() {
+  Serial.begin(115200);
+  while (CAN_OK != CAN.begin(CAN_250KBPS)) {
+    Serial.println("CAN INIT FAIL");
+    Serial.println("TRY AGAIN");
+    delay(1000);
+  }
+  Serial.println("Initialization Success");
+}
+
+void loop() {
+  if (CAN_MSGAVAIL == CAN.checkReceive()) {
+    CAN.readMsgBuf(&len, buf);
+    id = CAN.getCanId();
+    Serial.print("Getting Data from ID: 0x");
+    Serial.println(id, HEX);
+    for (int i = 0; i < len; i++) {
+      Serial.print(buf[i]);
+      Serial.print("\t");
+    }
+    Serial.println();
+  }
+}
