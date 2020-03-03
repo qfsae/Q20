0c6fc3bad774ea9068951c4a2e330dd2f6d24d64
Ethan Peterson <17emp5@queensu.ca>
2020-03-03 18:03:59 -0500

    add notes about CAN bus and code to decode messages

---
+++ CAN/motec-sniffer/README.md
@@ -0,0 +1,23 @@
+# Notes About CAN on Motec M150 ECU
+- "Channel" notation corresponds to the bytes at a given CAN address
+  * Ex. Channel 0 would correspond to the contents of index 0 in the CAN packet buffer
+- Screenshots containing the addressing info found in docs folder
+
+# Todo
+
+- Test for stability at a higher bit rate on the CAN bus
+  * Currently running at 250KBPS, aiming for 500KBPS or 1MBPS
+  * Motec Tune software produces warning for transmit overrun as our bit rate
+    is not fast enough to handle the frequency at which we have configured
+    certain messages to send at.
+  * Further optimization to increase performance is removing all Serial print
+    statements in the production code as prints take a long time as they are not
+    implemented asynchronously in the Arduino standard library thus the
+    functions dont exit until the bytes have been loaded into the UART send queue.
+- Address sensor faults for both fuel and coolant temp
+  * coolant currently reads 80 C (engine off)
+  * fuel reads (30 C) where as oil reads a more reasonable 21 C
+- Look into possible CAN configuration error with receive on the ECU from PDM
+  (can still observe the messages being sent from PDM on Arduino but not through
+  M1 Tune?)
+
--- CAN/motec-sniffer/motec-sniffer.ino
+++ CAN/motec-sniffer/motec-sniffer.ino
@@ -24,12 +24,74 @@ void loop() {
   if (CAN_MSGAVAIL == CAN.checkReceive()) {
     CAN.readMsgBuf(&len, buf);
     id = CAN.getCanId();
-    Serial.print("Getting Data from ID: 0x");
-    Serial.println(id, HEX);
-    for (int i = 0; i < len; i++) {
-      Serial.print(buf[i]);
-      Serial.print("\t");
+    /* Serial.print("Getting Data from ID: 0x"); */
+    /* Serial.println(id, HEX); */
+    /* for (int i = 0; i < len; i++) { */
+    /*   Serial.print(buf[i]); */
+    /*   Serial.print("\t"); */
+    /* } */
+    /* Serial.println(); */
+    if (id == 0x7FF) {
+      /* float voltage = buf[2] / 10.0; */
+      /* Serial.print("voltage: "); */
+      /* Serial.println(voltage); */
+    } else if (id == 0x118) {
+      /* Serial.print("RPM: "); */
+      /* Serial.println(buf[0] * 100); */
+
+      /* Serial.print("TPS: "); */
+      /* Serial.println(buf[1]); */
+
+      /* Serial.print("SPEED (km/h): "); */
+      /* Serial.println(buf[2]); */
+
+      /* Serial.print("COOLANT TEMP: "); */
+      /* Serial.println(buf[3]); */
+
+      /* Serial.print("OIL TEMP: "); */
+      /* Serial.println(buf[4]); */
+
+      /* Serial.print("FUEL TEMP: "); */
+      /* Serial.println(buf[5]); */
+
+      /* Serial.print("TRANSMISSION TEMP: "); */
+      /* Serial.println(buf[6]); */
+
+      /* Serial.print("DIFFERENTIAL TEMP: "); */
+      /* Serial.println(buf[7]); */
+    } else if (id == 0x119) {
+      Serial.print("FUEL PRESSURE (delta-kPa): ");
+      Serial.println(buf[0] * 256);
+
+      Serial.print("STEERING ANGLE: ");
+      Serial.println(buf[1] * 256 + buf[2]);
+
+      Serial.print("ENGINE STATE: ");
+      Serial.println(buf[3] & 0x1);
+
+      Serial.print("WARNING SOURCE: ");
+      Serial.println((buf[3] & 0x2) >> 1);
+
+      Serial.print("BRAKE STATE: ");
+      Serial.println((buf[3] & 0x4) >> 2);
+
+      Serial.print("ENGINE STATE: ");
+      Serial.println((buf[3] & 0x8) >> 3);
+
+      Serial.print("GEAR NEUTRAL SWITCH: ");
+      Serial.println((buf[3] & 0x10) >> 4);
+
+      Serial.print("CLUTCH SWITCH: ");
+      Serial.println((buf[3] & 0x20) >> 5);
+
+      Serial.print("CLUTCH STATE: ");
+      Serial.println((buf[3] & 0x40) >> 6);
+
+      Serial.print("DRIVER PIT SWITCH: ");
+      Serial.println((buf[3] & 0x80) >> 7);
+
+      Serial.print("ENGINE RUN SWITCH: ");
+      Serial.println((buf[4] & 0x1) >> 8);
     }
-    Serial.println();
   }
 }
