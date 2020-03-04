190659fca067314ecbd8a3c49d33bdc6608e2093
Ethan Peterson <17emp5@queensu.ca>
2020-03-03 19:19:46 -0500

    add PDM user manual and fixup base CAN address
    also adds code for reading all ECU messages we currently have access to

--- CAN/motec-sniffer/motec-sniffer.ino
+++ CAN/motec-sniffer/motec-sniffer.ino
@@ -31,10 +31,16 @@ void loop() {
     /*   Serial.print("\t"); */
     /* } */
     /* Serial.println(); */
-    if (id == 0x7FF) {
-      /* float voltage = buf[2] / 10.0; */
-      /* Serial.print("voltage: "); */
-      /* Serial.println(voltage); */
+    if (id == 0x7F0) {
+      float voltage = buf[2] * 0.1216;
+      Serial.print("VOLTAGE: ");
+      Serial.println(voltage);
+
+      Serial.print("FAN STATUS ACTIVE: ");
+      Serial.println(buf[0]);
+
+      Serial.print("PDM GLOBAL ERROR: ");
+      Serial.println(buf[1]);
     } else if (id == 0x118) {
       /* Serial.print("RPM: "); */
       /* Serial.println(buf[0] * 100); */
@@ -60,38 +66,51 @@ void loop() {
       /* Serial.print("DIFFERENTIAL TEMP: "); */
       /* Serial.println(buf[7]); */
     } else if (id == 0x119) {
-      Serial.print("FUEL PRESSURE (delta-kPa): ");
-      Serial.println(buf[0] * 256);
+      /* Serial.print("FUEL PRESSURE (delta-kPa): "); */
+      /* Serial.println(buf[0] * 10); */
 
-      Serial.print("STEERING ANGLE: ");
-      Serial.println(buf[1] * 256 + buf[2]);
+      /* Serial.print("STEERING ANGLE: "); */
+      /* Serial.println(buf[2] * 256 + buf[3]); */
 
-      Serial.print("ENGINE STATE: ");
-      Serial.println(buf[3] & 0x1);
+      /* Serial.print("ENGINE STATE: "); */
+      /* Serial.println(buf[4] & 0x1); */
 
-      Serial.print("WARNING SOURCE: ");
-      Serial.println((buf[3] & 0x2) >> 1);
+      /* Serial.print("WARNING SOURCE: "); */
+      /* Serial.println((buf[4] & 0x2) >> 1); */
 
-      Serial.print("BRAKE STATE: ");
-      Serial.println((buf[3] & 0x4) >> 2);
+      /* Serial.print("BRAKE STATE: "); */
+      /* Serial.println((buf[4] & 0x4) >> 2); */
 
-      Serial.print("ENGINE STATE: ");
-      Serial.println((buf[3] & 0x8) >> 3);
+      /* Serial.print("ENGINE STATE: "); */
+      /* Serial.println((buf[4] & 0x8) >> 3); */
 
-      Serial.print("GEAR NEUTRAL SWITCH: ");
-      Serial.println((buf[3] & 0x10) >> 4);
+      /* Serial.print("GEAR NEUTRAL SWITCH: "); */
+      /* Serial.println((buf[4] & 0x10) >> 4); */
 
-      Serial.print("CLUTCH SWITCH: ");
-      Serial.println((buf[3] & 0x20) >> 5);
+      /* Serial.print("CLUTCH SWITCH: "); */
+      /* Serial.println((buf[4] & 0x20) >> 5); */
 
-      Serial.print("CLUTCH STATE: ");
-      Serial.println((buf[3] & 0x40) >> 6);
+      /* Serial.print("CLUTCH STATE: "); */
+      /* Serial.println((buf[4] & 0x40) >> 6); */
 
-      Serial.print("DRIVER PIT SWITCH: ");
-      Serial.println((buf[3] & 0x80) >> 7);
+      /* Serial.print("DRIVER PIT SWITCH: "); */
+      /* Serial.println((buf[4] & 0x80) >> 7); */
 
-      Serial.print("ENGINE RUN SWITCH: ");
-      Serial.println((buf[4] & 0x1) >> 8);
+      /* Serial.print("ENGINE RUN SWITCH: "); // May be switched with engine
+       * state */
+      /* Serial.println((buf[5] & 0x1)); */
+
+      /* Serial.print("DRIVER SWITCH 1"); */
+      /* Serial.println((buf[5] & 0x2) >> 1); */
+
+      /* Serial.print("DRIVER SWITCH 2"); */
+      /* Serial.println((buf[5] & 0x4) >> 2); */
+
+      /* Serial.print("DRIVER ROTARY SWITCH 1"); */
+      /* Serial.println(buf[6]); */
+
+      /* Serial.print("DRIVER ROTARY SWITCH 2"); */
+      /* Serial.println(buf[7]); */
     }
   }
 }
