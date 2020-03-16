b34c28cb75f96417ccd626313fe7e1916abbdeec
Ethan Peterson <17emp5@queensu.ca>
2020-03-16 01:53:32 -0400

    teensy screen code working on COBS

--- dash/dash-pcb-test-teensy-screen/dash-pcb-test-teensy-screen.ino
+++ dash/dash-pcb-test-teensy-screen/dash-pcb-test-teensy-screen.ino
@@ -2,36 +2,54 @@
 #include <TimeLib.h>
 
 #include <EEPROM.h>
-#include <GD2.h>
 #include <SPI.h>
+#include <GD2.h>
 
 #define BAUD_RATE 1000000
 #define USerial Serial1
 #define BODY_LENGTH 4
+#define ENCODED_LENGTH BODY_LENGTH + 2
 
 IntervalTimer timer;
 
 volatile unsigned char n = 0;
-volatile unsigned char currentMsg[BODY_LENGTH];
-volatile unsigned char data[BODY_LENGTH + 2];
+volatile uint8_t currentMsg[BODY_LENGTH];
+volatile uint8_t data[ENCODED_LENGTH];
 volatile int tps;
 
 bool canStarted = false;
 
-void arrCpy(unsigned char *src, unsigned char *dest) {
-  for (int i = 0; i < BODY_LENGTH; i++) {
-    dest[i] = src[i];
-  }
+void timerInterrupt() {
+  recvData();  
 }
 
-void timerInterrupt() { recvData(); }
+volatile uint8_t fcobs_decode(volatile uint8_t *data, volatile uint8_t *dest, volatile uint8_t data_len) {
+  const volatile uint8_t *start = dest, *end = data + data_len;
+  volatile uint8_t code = 0xFF, copy = 0;
+
+  for (; data < end; copy--) {
+    if (copy != 0) {
+      *dest++ = *data++;
+    } else {
+      if (code != 0xFF)
+        *dest++ = 0;
+      copy = code = *data++;
+      if (code == 0)
+        break; /* Source length too long */
+    }
+  }
+  return dest - start;
+}
 
 void recvData() {
+  int endMarker = -20000;
+  char *endBytes = (char *)&endMarker;
   if (USerial.available()) {
-    uint8_t a = USerial.read();
+    char a = USerial.read();
     if (a == 0) {
+      // The message has ended
+      fcobs_decode(data, currentMsg, ENCODED_LENGTH);
       n = 0;
-      fcobs_decode(data, currentMsg, BODY_LENGTH + 2);
       if (currentMsg[3] == 0x01) {
         tps = currentMsg[1];
       }
@@ -48,20 +66,20 @@ void printCurrentMsg() {
     Serial.print('\t');
   }
   Serial.println();
-  Serial.println("END");
+  Serial.println("END");  
 }
 
 void screenUpdate() {
-  GD.ClearColorRGB(0, 0, 0);
+  GD.ClearColorRGB(0,0,0);
   GD.Clear();
   GD.cmd_gauge(150, 136, 100, OPT_NOPOINTER, 8, 5, 0, 7500);
-  GD.ColorRGB(255, 0, 0);
+  GD.ColorRGB(255,0,0);
   GD.cmd_gauge(150, 136, 100, OPT_NOBACK | OPT_NOTICKS, 14, 5, tps, 7500);
-  GD.ColorRGB(255, 255, 255);
-  //  GD.cmd_text(25, 25, 25, 0, receivedChars);
+  GD.ColorRGB(255,255,255);
+//  GD.cmd_text(25, 25, 25, 0, receivedChars);
   GD.cmd_text(130, 240, 24, 0, "RPM");
   GD.cmd_number(150, 180, 24, OPT_CENTER, tps);
-  GD.cmd_number(350, 136, 31, OPT_CENTER, tps);
+  GD.cmd_number(350, 136, 31, OPT_CENTER, tps); 
   GD.swap();
 }
 
@@ -71,9 +89,9 @@ void startScreen() {
   GD.Vertex2ii(240 - 200, 40);
   GD.cmd_text(240, 260, 21, OPT_CENTER, "Waiting for CAN bus");
   GD.End();
-
+  
   GD.cmd_spinner(240, 220, 1, 0);
-
+  
   GD.swap();
 }
 
@@ -94,10 +112,11 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (canStarted) {
     screenUpdate();
+    //recvData();
   } else {
     startScreen();
     if (USerial.available()) {
       canStarted = true;
-    }
+    }  
   }
 }
