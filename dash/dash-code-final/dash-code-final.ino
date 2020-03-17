51f6744f6364efc1641dbb1ca724aa4b2fcee11f
Ethan Peterson <17emp5@queensu.ca>
2020-03-16 21:36:29 -0400

    added struct to retreive can data from

--- dash/dash-code-final/dash-code-final.ino
+++ dash/dash-code-final/dash-code-final.ino
@@ -13,6 +13,9 @@
 
 #define INTERRUPT_PRIORITY 0
 
+// the factor voltage values from CAN must be scaled by
+#define VOLTAGE_FACTOR 0.1216
+
 struct CANData {
   int rpm;
   uint8_t tps;
@@ -20,6 +23,13 @@ struct CANData {
   int speed; // in km/h
   bool pdmError;
   bool fanActive;
+  uint8_t fuelTemp;
+  uint8_t oilTemp;
+  uint8_t coolantTemp;
+  uint8_t differentialTemp;
+  uint8_t transmissionTemp;
+  int fuelPressure; // in delta kpa
+  int steeringAngle; // in degrees
 };
 
 IntervalTimer timer;
@@ -27,9 +37,39 @@ IntervalTimer timer;
 volatile unsigned char n = 0;
 volatile uint8_t currentMsg[BODY_LENGTH];
 volatile uint8_t data[ENCODED_LENGTH];
-volatile int tps;
 
-volatile bool canStarted = false;
+volatile struct CANData can;
+volatile uint8_t sNumber = 0;
+volatile bool canStarted;
+
+// takes the UART ID of the packet and updates the related fields in the struct
+void updateCANData(uint8_t idUART) {
+  switch (idUART) {
+    case 0x10:
+      can.speed = currentMsg[2];
+      can.tps = currentMsg[1];
+      can.rpm = currentMsg[0] * 100;
+    break;
+    case 0x20:
+      can.fuelTemp = currentMsg[2];
+      can.oilTemp = currentMsg[1];
+      can.coolantTemp = currentMsg[0];
+    break;
+    case 0x30:
+      can.differentialTemp = currentMsg[2];
+      can.transmissionTemp = currentMsg[1];
+    break;
+    case 0x02:
+      can.fuelPressure = currentMsg[0] * 10;
+      can.steeringAngle = currentMsg[2] * 256 + currentMsg[1];
+    break;
+    case 0x03:
+      can.voltage = (float)currentMsg[2] * VOLTAGE_FACTOR;
+      can.pdmError = currentMsg[1];
+      can.fanActive = currentMsg[0];
+    break;
+  }
+}
 
 void timerInterrupt() {
   recvData();
@@ -41,12 +81,8 @@ void recvData() {
     if (a == 0) {
       // The message has ended
       fcobs_decode(data, currentMsg, ENCODED_LENGTH);
+      updateCANData(currentMsg[3]);
       n = 0;
-      //Serial.println(currentMsg[0]);
-      
-      if (currentMsg[3] == 0x10) {
-        tps = currentMsg[1];
-      }
     } else {
       data[n] = a;
       n++;
@@ -78,12 +114,9 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (canStarted) {
-    screenUpdate();
-  } else {
-    startScreen();
-    if (USerial.available()) {
-      canStarted = true;
-    }
+  if (USerial.available() && !canStarted) {
+    canStarted = true;
+    sNumber = 1;
   }
+  screenUpdate(sNumber);
 }
